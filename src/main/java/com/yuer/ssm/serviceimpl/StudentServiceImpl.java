836d840b949cf30774ffe1eced8f6e054716edc7
yuyahao <yuyahao@futurelab.tv>
2019-05-29 18:00:18 +0800

    ssm框架整合完毕，成功运行，over !

--- src/main/java/com/yuer/ssm/serviceimpl/StudentServiceImpl.java
+++ src/main/java/com/yuer/ssm/serviceimpl/StudentServiceImpl.java
@@ -4,6 +4,7 @@ import com.yuer.ssm.bean.Student;
 import com.yuer.ssm.dao.IStudentDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
